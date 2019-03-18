3ea3d74fc23bfb7e103807f24233cbbe85e68059
Karra <xp0729@gmail.com>
2019-03-18 15:22:19 +1100

    refactor filter data by duration

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ActiveRecord::Base
   default_scope {order('record_at desc')}
+
   belongs_to :bill
   belongs_to :parent_type, foreign_key: 'parent_type_id', class_name: 'ItemType'
   belongs_to :child_type, foreign_key: 'child_type_id', class_name: 'ItemType'
+
+  TODAY = Time.current
 
   enum inorout: {
     expense: 0,
@@ -10,25 +13,29 @@ class Item < ActiveRecord::Base
   }
 
   def self.year
-    where('extract(year from record_at) = ?', Date.current.year)
+    filter_data_by_duration(TODAY.at_beginning_of_year, TODAY.at_end_of_year)
   end
 
   def self.month(date=nil)
-    date ||= Date.current
-    where('extract(year from record_at) = ? AND extract(month from record_at) = ?', date.year, date.month)
+    date ||= TODAY
+    filter_data_by_duration(date.at_beginning_of_month, date.at_end_of_month)
   end
 
   def self.quarter
-    where('record_at > ?', Time.current.at_beginning_of_month - 2.months)
+    date = TODAY.at_end_of_month
+    filter_data_by_duration(date - 2.months, date)
+    where('record_at > ?', TODAY.at_beginning_of_month - 2.months)
   end
 
   def self.week
-    date = Date.current
-    where('extract(year from record_at) = ? AND extract(week from record_at) = ?', date.year, date.cweek)
+    filter_data_by_duration(TODAY.at_beginning_of_week, TODAY.at_end_of_week)
   end
 
   def self.day
-    date = Date.current
-    where('extract(year from record_at) = ? AND extract(doy from record_at) = ?', date.year, date.yday)
+    filter_data_by_duration(TODAY.at_beginning_of_day, TODAY.at_end_of_day)
+  end
+
+  def self.filter_data_by_duration(time_from, time_to)
+    where('record_at >= ? and record_at <= ?', time_from, time_to)
   end
 end
